80d9ba6fbbba9647afbadc943b173dbd63055ce8
Adam Kater <adam.kater@rgi-corp.com>
2018-09-11 15:05:07 -0400

    Moved srs to the stack. It's light weight. Also avoids being deleted by the wrong delete operator.

--- GIP/GeoRaster.cpp
+++ GIP/GeoRaster.cpp
@@ -264,10 +264,9 @@ namespace gip {
         if (feature.valid()) {
             OGRGeometry* site = feature.ogr_geometry();
             // if imgout srs different than feature srs
-            OGRSpatialReference* srs = new OGRSpatialReference;
-            srs->SetFromUserInput(imgout.srs().c_str());
-            site->transformTo(srs);
-            OGRSpatialReference::DestroySpatialReference(srs);
+			OGRSpatialReference srs;
+            srs.SetFromUserInput(imgout.srs().c_str());
+            site->transformTo(&srs);
 
             // Create cutline transform to pixel coordinates        
             papszOptionsCutline = CSLSetNameValue( papszOptionsCutline, "DST_SRS", imgout.srs().c_str() );
